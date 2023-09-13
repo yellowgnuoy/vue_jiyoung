74161df8d4b0dbbd29ac955d4b2131c891ad77e9
yellowgnuoy <116864621+yellowgnuoy@users.noreply.github.com>
2023-09-13 17:24:50 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <div>
+    <h4 class="red" :style="스타일">XX 원룸</h4>
+    <p>{{ price1 }} 만원</p>
+  </div>
+    <div>
+    <h4>XX 원룸</h4>
+    <p>{{ price2 }} 만원</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  data(){
+    return {
+      price1 :80,
+      price2 :70,
+      스타일 : 'color : blue',
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
